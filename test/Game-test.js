c9cfd3fedac9250be5bc230ab8d9c02fa060192f
Ivy Nguyen <animeweirdo24@gmail.com>
2022-07-01 12:36:57 -0600

    Complete the game class using TDD

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai')
 const expect = chai.expect
 
-const data = require('./data');
+const data = require('../src/data');
 const prototypeQuestions = data.prototypeData;
 const Card = require('../src/Card')
 const Deck = require('../src/Deck')
@@ -11,41 +11,42 @@ const Game = require('../src/Game')
 
 describe('Game', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const game = new Game()
     expect(Game).to.be.a('function')
   })
 
-  it.skip('should be an instance of Game', function() {
+  it('should be an instance of Game', function() {
     const game = new Game()
     expect(game).to.be.an.instanceof(Game)
   })
 
-  it.skip('should start the game', function() {
+  it('should start the game', function() {
+    const game = new Game()
+
+    expect(game.start).to.be.a('function')
+  })
+
+  it('should create cards when started', function() {
     const game = new Game()
 
     game.start()
-
-    expect(game.start()).to.be.a('function')
-  })
-
-  it.skip('should create cards when started', function() {
-    const game = new Game()
-
     game.createCards()
 
     expect(game.currentRound.deck.currentDeck[0]).to.be.an.instanceof(Card)
   })
 
-  it.skip('should create a new deck', function() {
+  it('should create a new deck', function() {
     const game = new Game()
 
+    game.start()
+    game.createCards()
     game.createDeck()
 
     expect(game.currentRound.deck).to.be.an.instanceof(Deck)
   })
 
-  it.skip('should create a new round', function() {
+  it('should create a new round', function() {
     const game = new Game()
 
     game.createRound()
